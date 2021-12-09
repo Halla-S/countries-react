b3f7202ea984ee418cf3f3f6882cee3c6c07854f
Halla-S <86550752+Halla-S@users.noreply.github.com>
2021-12-09 19:09:19 +0000

    select list added

--- src/App.js
+++ src/App.js
@@ -2,43 +2,52 @@ import React, { useState } from "react";
 import "./App.css";
 import Country from "./Country";
 import Data from "./allCountries.json";
-
+import Options from "./Options";
 
 function App() {
-   let [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [country, setCountry] = useState(Data);
   return (
     <div>
-      <input
-        type="text"
-        placeholder="search"
-        onChange={(event) => setSearchTerm(event.target.value)}
-      ></input>
-      <div className="main">
-        {Data.filter((country) => {
+      <div className="select-search">
+        <input
+          type="text"
+          placeholder="search"
+          onChange={(event) => setSearchTerm(event.target.value)}
+        ></input>
+        <Options data={Data} onSelecting={(country) => setCountry(country)} />{" "}
+        {/* why this gives me error of too many renders if I put it this way (onChangeProp={setCountry(country))} */}
+      </div>
+      <div>
+        <div className="main">
+          {country
+            .filter((country) => {
               if (searchTerm === "") {
                 return country;
               } else if (
                 country.name
                   .toLowerCase()
-                  .includes(searchTerm.toLocaleLowerCase())||country.capital.toLowerCase()
+                  .includes(searchTerm.toLocaleLowerCase()) ||country.capital
+                  .toLowerCase()
                   .includes(searchTerm.toLocaleLowerCase())
               ) {
                 return country;
-              }else{
+              } else {
                 return false;
               }
             })
-        .map((country) => {
-          return (
-            <Country
-              Flag={country.flag}
-              Name={country.name}
-              Population={country.population}
-              Capital={country.capital}
-              Region={country.region}
-            />
-          );
-        })}
+            .map((country) => {
+              return (
+                <Country
+                  Flag={country.flag}
+                  Name={country.name}
+                  Population={country.population}
+                  Capital={country.capital}
+                  Region={country.region}
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
--- src/Country.js
+++ src/Country.js
@@ -4,14 +4,14 @@ import "./App.css";
 const Country = (props) => {
   return (
     <div className="country-info">
-      <img src={props.Flag} alt="" />
-      <h5>{props.Name}</h5>
-      <ul>
-        <li key={3}>Population: {props.Population}</li>
-        <li key={4}>Region: {props.Region}</li>
-        <li key={5}>Capital: {props.Capital}</li>
-      </ul>
-    </div>
+        <img src={props.Flag} alt="" />
+        <h5>{props.Name}</h5>
+        <ul>
+          <li key={3}>Population: {props.Population}</li>
+          <li key={4}>Region: {props.Region}</li>
+          <li key={5}>Capital: {props.Capital}</li>
+        </ul>
+      </div>
   );
 };
 export default Country;
